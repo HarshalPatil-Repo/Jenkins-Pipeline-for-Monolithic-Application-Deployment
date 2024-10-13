68cc3371b93e6b91cb641c7344e291e509e28ccb
Harshal Pundlik Patil <hpharshalmech@gmail.com>
2024-10-13 19:56:44 +0530

    Update Jenkins_Pipeline.groovy

--- Jenkins_Pipeline.groovy
+++ Jenkins_Pipeline.groovy
@@ -1,10 +1,26 @@
+
 pipeline {
     agent any
+
+    environment {
+        // Define version using BUILD_NUMBER and an optional custom suffix
+        NEW_VERSION = "8.2.${BUILD_NUMBER}-SNAPSHOT"
+    }
+
 
     stages {
         stage('Git Checkout') {
             steps {
                 git 'https://github.com/HarshalPatil-Repo/Jenkins-Pipeline-for-Monolithic-Application-Deployment.git'
+            }
+        }
+        stage('Version Change') {
+            steps {
+                sh """
+                        mvn versions:set -DnewVersion=${NEW_VERSION}
+                    """
+                // Confirm the version has been updated
+                sh "cat pom.xml"
             }
         }
         stage('Sonar Code Testing') {
@@ -19,18 +35,18 @@ pipeline {
         }
         stage('Nexus Artifact Upload') {
             steps {
-                nexusArtifactUploader artifacts: [[artifactId: 'myweb', classifier: '', file: 'target/myweb-8.2.17-SNAPSHOT.war', type: 'war']], credentialsId: 'nexus3', groupId: 'in.javahome', nexusUrl: '172.31.86.205:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-snapshots', version: '8.2.17-SNAPSHOT'
+                nexusArtifactUploader artifacts: [[artifactId: 'myweb', classifier: '', file: "target/myweb-${NEW_VERSION}.war", type: 'war']], credentialsId: 'nexus3', groupId: 'in.javahome', nexusUrl: '44.211.139.20:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-snapshots', version: "${NEW_VERSION}"
             }
         }
         stage('Tomcat Deployemnt') {
             steps {
                 sh """
-                  ssh jenkins@172.31.86.100 sudo rm -rf /var/tmp/*.war
-                  ssh jenkins@172.31.86.100 sudo rm -rf /opt/apache-tomcat-9.0.54/webapps/*.war
-                  scp /var/lib/jenkins/workspace/MonoliticApplicatonDeployment/target/*.war jenkins@172.31.86.100:/var/tmp
-                  ssh jenkins@172.31.86.100 sudo cp /var/tmp/*.war /opt/apache-tomcat-9.0.54/webapps/
-                  ssh jenkins@172.31.86.100 sudo tomdown
-                  ssh jenkins@172.31.86.100 sudo tomup
+                  ssh jenkins@172.31.91.73 sudo rm -rf /var/tmp/*.war
+                  ssh jenkins@172.31.91.73 sudo rm -rf /opt/apache-tomcat-9.0.96/webapps/myweb-8.2.*
+                  scp /var/lib/jenkins/workspace/MonoliticApplicatonDeployment/target/*.war jenkins@172.31.91.73:/var/tmp
+                  ssh jenkins@172.31.91.73 sudo cp /var/tmp/*.war /opt/apache-tomcat-9.0.96/webapps/
+                  ssh jenkins@172.31.91.73 sudo tomdown
+                  ssh jenkins@172.31.91.73 sudo tomup
                 """
             }
         }
